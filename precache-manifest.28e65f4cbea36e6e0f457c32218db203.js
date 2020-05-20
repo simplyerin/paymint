ec164da84297959b018b7f1c5919979a98bda065
simplyerin <65418908+simplyerin@users.noreply.github.com>
2020-05-20 00:04:05 -0700

    Updates

---
+++ precache-manifest.28e65f4cbea36e6e0f457c32218db203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b261fc1c24f18968a2caa178334f372",
+    "url": "/paymint/index.html"
+  },
+  {
+    "revision": "36213c63c2a206bda67a",
+    "url": "/paymint/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae97962bd4c517dc83d4",
+    "url": "/paymint/static/js/2.4667c2e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paymint/static/js/2.4667c2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36213c63c2a206bda67a",
+    "url": "/paymint/static/js/main.b61020b4.chunk.js"
+  },
+  {
+    "revision": "19859ad0f4196de66210",
+    "url": "/paymint/static/js/runtime-main.2ac003e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paymint/static/media/logo.5d5d9eef.svg"
+  }
+]);
